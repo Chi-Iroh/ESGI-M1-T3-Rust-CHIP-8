0885af2e72d400ef55931b810440cb1c39ce2a6d
Prime-arys <88397986+Prime-arys@users.noreply.github.com>
2026-06-28 18:41:22 +0200

    feat: add function to load CHIP-8 program from file
    - wrap errors from Engine and Interpreter into AppError, this permit to add the IO error from std::fs::read() in the same error type

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,17 @@
 use crate::engine::{Engine, EngineError};
 use crate::interpreter::{Interpreter, InterpreterError};
+use std::path::Path;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum AppError {
+    #[error("Engine error: {0}")]
+    Engine(#[from] EngineError),
+    #[error("Interpreter error: {0}")]
+    Interpreter(#[from] InterpreterError),
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
+}
 
 pub struct App {
     engine: Engine,
@@ -15,13 +27,22 @@ impl App {
         }
     }
 
-    pub fn run(&mut self) -> Result<(), InterpreterError> {
-        self.interpreter.run(&mut self.engine)
+    pub fn run(&mut self) -> Result<(), AppError> {
+        self.interpreter.run(&mut self.engine)?;
+        Ok(())
     }
 
-    pub fn new_from_byte(program: &[u8]) -> Result<Self, EngineError> {
+    pub fn _new_from_byte(program: &[u8]) -> Result<Self, AppError> {
         let mut app = App::new();
         app.engine.load_program(program)?;
+        Ok(app)
+    }
+
+    pub fn new_from_file<P: AsRef<Path>>(path: P) -> Result<Self, AppError> {
+        // https://doc.rust-lang.org/beta/std/fs/fn.read.html
+        let program = std::fs::read(path)?;
+        let mut app = App::new();
+        app.engine.load_program(&program)?;
         Ok(app)
     }
 
