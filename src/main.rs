bb72f8a69c56be8cdbe2bf56e597d3eb9460859a
Prime-arys <88397986+Prime-arys@users.noreply.github.com>
2026-06-28 18:48:57 +0200

    feat: update main to execute CHIP-8 program from file and display registers before and after execution
    - it could be used this way: `cargo run -- -i .\program.ch8`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+mod app;
+mod cli;
+mod engine;
+mod interpreter;
+
+use app::App;
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let args = cli::Args::parse();
+
+    let mut app = match args.input {
+        Some(path) => match App::new_from_file(path) {
+            Ok(app) => app,
+            Err(e) => {
+                eprintln!("Error loading program from file: {}", e);
+                return;
+            }
+        },
+        None => {
+            eprintln!("No input file provided. Please specify a CHIP-8 program file.");
+            return;
+        }
+    };
+
+    app.draw_current_registers();
+    match app.run() {
+        Ok(_) => println!("Program executed successfully."),
+        Err(e) => eprintln!("Error during execution: {}", e),
+    }
+    app.draw_current_registers();
 }
